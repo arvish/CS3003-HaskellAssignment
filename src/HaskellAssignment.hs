986b0f4ffc68e76a40e11e51e05d66025b484c85
Arvish Pandey <arvishpandey@icloud.com>
2024-12-12 16:56:30 -0500

    update

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -3,22 +3,20 @@ module HaskellAssignment where
 ------------------------------------------------
 -- findFirst
 ------------------------------------------------
-data Found = Match Int | NoMatch deriving Eq
 
-instance Show Found where
-  show (Match index) = "(Match " ++ show index ++ ")"
-  show NoMatch       = "NoMatch"
+data Found = Match Int | NoMatch deriving (Eq)
 
 findFirst :: (a -> Bool) -> [a] -> Found
 findFirst predicate xs = go 0 xs
   where
     go _ []     = NoMatch
-    go i (y:ys) = if predicate y
-                  then Match i
-                  else go (i + 1) ys
+    go i (y:ys)
+      | predicate y = Match i
+      | otherwise    = go (i + 1) ys
 
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
+
 palindrome :: [Char] -> Bool
 palindrome candidate = candidate == reverse candidate
