06b88a12e07eeab0d0f2bbbaa985cbedcc9cf71f
Arvish Pandey <arvishpandey@icloud.com>
2024-12-12 16:30:53 -0500

    debug format commit

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -3,17 +3,12 @@ module HaskellAssignment where
 ------------------------------------------------
 -- findFirst
 ------------------------------------------------
-
--- The Found data type to represent whether a match has been found, and at which index
 data Found = Match Int | NoMatch deriving Eq
 
 instance Show Found where
-  show (Match index) = "Found match at " ++ show index
-  show NoMatch = "No match found!"
+  show (Match index) = "(Match " ++ show index ++ ")"
+  show NoMatch       = "NoMatch"
 
--- findFirst takes a predicate and a list and returns Found.
--- If the predicate is satisfied by an element, return Match with that element's index.
--- If no element satisfies the predicate, return NoMatch.
 findFirst :: (a -> Bool) -> [a] -> Found
 findFirst predicate xs = go 0 xs
   where
@@ -25,8 +20,5 @@ findFirst predicate xs = go 0 xs
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
-
--- A palindrome function that checks if a given string is the same forwards and backwards.
--- The empty string is considered a palindrome.
 palindrome :: [Char] -> Bool
 palindrome candidate = candidate == reverse candidate
